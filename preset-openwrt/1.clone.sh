a078939877eef9e3ccea5302cb74b5ec51b72140
hty-workspace <71050972+hty-workspace@users.noreply.github.com>
2023-06-07 17:58:55 +0800

    test

--- preset-openwrt/1.clone.sh
+++ preset-openwrt/1.clone.sh
@@ -19,5 +19,6 @@ mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/yichya/luci-app-xray.git
+git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git
 git clone --depth 1 -b packages https://github.com/xiaorouji/openwrt-passwall.git pw-dependencies
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
